59db2003731aa1dd4f2381bc5d621f224f9daaa6
ricardo-719 <teacherrickyricardo@gmail.com>
2022-11-25 13:13:57 +0700

    update header image

--- Ecommerce-site/src/routes/index.tsx
+++ Ecommerce-site/src/routes/index.tsx
@@ -16,7 +16,8 @@ export default component$(() => {
     <div class="flex flex-1 flex-col">
       {/* Background image */}
       <section class="min-h-screen flex relative">
-        <img src="https://escrituras-eremitas.com/wp-content/uploads/2022/11/1792305-scaled.jpg"
+        {/* Alternative header image: <img src="https://escrituras-eremitas.com/wp-content/uploads/2022/11/1792305-scaled.jpg"*/}
+        <img src="https://images.pexels.com/photos/2829367/pexels-photo-2829367.jpeg"
         alt="person at the pool" class="object-cover"/>
         <a href="#products" class="absolute left-1/2 -translate-x-1/2 top-2/3 -translate-y-1/2 p-4 px-8 border-2 
         text-white border-solid border-white hover:text-slate-900 after:absolute after:bg-white after:right-full
